af1effa07eb32dfefba48e883c85cb30c98cfdd6
Mauro Bringolf <maurobringolf@users.noreply.github.com>
2017-12-12 14:21:46 +0100

    [testing] Start basic test coverage

---
+++ tests/test-loader.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Class LoaderTest
+ *
+ * @package Google_Analytics
+ */
+
+/**
+ * Loader test case.
+ */
+class LoaderTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->plugin = WebKinder\GoogleAnalytics\PluginFactory::create();
+	}
+
+	/**
+	 * A single example test.
+	 */
+	function test_should_track_visit() {
+
+		// Logged out should always be tracked
+		wp_logout();
+
+		update_option('track_logged_in', 1);
+		$this->assertTrue($this->plugin->loader->should_track_visit());
+
+		update_option('track_logged_in', 0);
+		$this->assertTrue($this->plugin->loader->should_track_visit());
+
+		// Logged in should only be tracked when option is set
+		$user_id = $this->factory->user->create();
+		wp_set_current_user( $user_id );
+
+		update_option('track_logged_in', 1);
+		$this->assertTrue($this->plugin->loader->should_track_visit());
+
+		update_option('track_logged_in', 0);
+		$this->assertFalse($this->plugin->loader->should_track_visit());
+	}
+}
