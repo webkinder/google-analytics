1ce465fb657b3506d7c2f63385643efd17fe3440
Pascal Knecht <pascal.knecht99@gmail.com>
2018-05-23 18:16:42 +0200

    Added possibility to add text to privacy page

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -3,7 +3,7 @@
 namespace WebKinder\GoogleAnalytics;
 
 class Plugin {
-  
+
   public function run() {
 
     //i18n
@@ -12,14 +12,14 @@ class Plugin {
   	//settings
     include_once 'Settings.php';
     $this->settings = new Settings();
-    
+
     add_action( 'admin_init', array( $this->settings, 'register_settings' ) );
     add_action( 'admin_menu', array( $this->settings, 'settings_page' ) );
 
     //loader
     include_once 'Loader.php';
     $this->loader = new Loader();
-    
+
   	//cookie handling
   	add_action( 'admin_enqueue_scripts', array( $this->loader, 'load_admin_scripts' ) );
     //cookie function
@@ -36,6 +36,7 @@ class Plugin {
     add_filter( 'plugin_row_meta', array( $this, 'additional_admin_information_links' ), 10, 2);
     add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'additional_admin_action_links' ) );
 
+    add_action( 'admin_init', array($this, 'add_privacy_policy_content') );
   }
 
 
@@ -47,15 +48,15 @@ class Plugin {
    *
    */
   function additional_admin_information_links( $links, $file ) {
-    
+
     if (dirname($file) == basename(WK_GOOGLE_ANALYTICS_DIR)) {
       $links[] = '<a href="http://bit.ly/2jnKboN">' . __('Donate to this plugin', 'wk-google-analytics') . '</a>';
     }
-    
+
     return $links;
-    
+
   }
-  
+
 
   /**
    * Sets up the translations in /lang directory
@@ -66,6 +67,18 @@ class Plugin {
 	function load_textdomain() {
 		load_plugin_textdomain( 'wk-google-analytics', false, plugins_url(plugin_basename(WK_GOOGLE_ANALYTICS_DIR)) . '/lang/' );
 	}
+
+	/**
+	 * Add privacy policy to WordPress privacy policy
+	 */
+	function add_privacy_policy_content(){
+		if( !function_exists( 'wp_add_privacy_policy_content' ) ) {
+			return;
+		}
+		$content = wpautop( __( 'Your google analytics content here', 'wk-google-analytics' ) );
+		wp_add_privacy_policy_content( 'Google Analytics', $content );
+	}
+
 
 
 }
