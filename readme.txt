3fc911b64c1974c3385ea169a52de022e7437c37
pascalknecht <pascal@webkinder.ch>
2020-02-11 10:46:18 +0100

    Fixed readme

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Google Analytics ===
 Contributors: webkinder
 Tags: google analytics, tracking code, analytics, anonymization, anonymize, anonymizeIp, cookie, Datenschutz, ga, gaoptout, google, googleanalytics, google tag manager, gtm, Datenschutz, datenschutzkonform, script, snippet
-Requires at least: 3.0
-Requires PHP: 7.1
+Requires at least: 4.8
 Tested up to: 5.3.2
+Requires PHP: 7.1
 Stable tag: 1.8.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
--- wk-ga.php
+++ wk-ga.php
@@ -1,16 +1,16 @@
 <?php
-/*
-Plugin Name: Google Analytics by WebKinder
-Plugin URI:  https://wordpress.org/plugins/wk-google-analytics/
-Description: Google Analytics for WordPress without tracking your own visits
-Version:     1.8.0
-Author:      WebKinder
-Author URI:  https://www.webkinder.ch
-License:     GPL2
-License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Domain Path: /languages
-Text Domain: wk-google-analytics
-*/
+/**
+ * Plugin Name: Google Analytics by WebKinder
+ * Plugin URI: https://wordpress.org/plugins/wk-google-analytics/
+ * Description: Google Analytics for WordPress without tracking your own visits
+ * Version: 1.8.0
+ * Author: WebKinder
+ * Author URI: https://www.webkinder.ch
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path: /languages
+ * Text Domain: wk-google-analytics
+ */
 
 
 define('WK_GOOGLE_ANALYTICS_DIR', dirname(__FILE__));
