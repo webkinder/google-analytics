56520beaf1abae353c1682ebc17ba744a4488ce4
svenvonarx <sven.vonarx@hotmail.de>
2020-09-10 15:13:12 +0200

    added test case for simple tracking code output check

--- tests/test-factory.php
+++ tests/test-factory.php
@@ -10,6 +10,38 @@
  */
 class FactoryTest extends WP_UnitTestCase {
 
+	private $_plugin_instance;
+
+	// Tests TODO:
+	// Check Output Type
+	// Check anonymisation
+	
+	public function setUp()
+	{
+		update_option('ga_tracking_code', 'UA-12345678'); 
+
+		$this->_plugin_instance = WebKinder\GoogleAnalytics\PluginFactory::create();
+
+		/*update_option('ga_tag_manager_id', ''); 
+		update_option('ga_anonymize_ip', ''); 
+		update_option('ga_use_tag_manager', '');*/
+	}
+
+	function test_tracking_code_output()
+	{
+		
+		ob_start();
+
+		wp_head();
+
+		$head_contents = ob_get_clean();
+
+		$output_analytics = strpos($head_contents, "id='wk-analytics-script-js-after'") > -1;
+
+		$this->assertTrue($output_analytics);
+
+	}
+
 	/**
 	 * A single example test.
 	 */
