d161a81ad8596f76857e1941b3cf7c60e6c51336
svenvonarx <sven.vonarx@hotmail.de>
2022-05-03 15:02:28 +0200

    trying to fix CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,8 @@
  * @package Google_Analytics
  */
 
+require dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
