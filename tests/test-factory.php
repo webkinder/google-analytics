d7eb4b57dba71ef051e6e0d2e4817ef234dcfa42
svenvonarx <sven.vonarx@hotmail.de>
2022-05-03 13:14:24 +0200

    remove 7.1 support

--- tests/test-factory.php
+++ tests/test-factory.php
@@ -15,21 +15,21 @@ class FactoryTest extends WP_UnitTestCase {
 	// Tests TODO:
 	// Check Output Type
 	// Check anonymisation
-	
+
 	public function setUp()
 	{
-		update_option('ga_tracking_code', 'UA-12345678'); 
+		update_option('ga_tracking_code', 'UA-12345678');
 
 		$this->_plugin_instance = WebKinder\GoogleAnalytics\PluginFactory::create();
 
-		/*update_option('ga_tag_manager_id', ''); 
-		update_option('ga_anonymize_ip', ''); 
+		/*update_option('ga_tag_manager_id', '');
+		update_option('ga_anonymize_ip', '');
 		update_option('ga_use_tag_manager', '');*/
 	}
 
 	function test_tracking_code_output()
 	{
-		
+
 		ob_start();
 
 		wp_head();
