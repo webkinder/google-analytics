1fe5f459984f12ccc02139e182409f1fbc668faf
silvanarnet <silvan@webkinder.ch>
2022-11-04 10:19:11 +0100

    Version bump

--- wk-ga.php
+++ wk-ga.php
@@ -4,21 +4,19 @@
  * Plugin Name: Google Analytics and Google Tag Manager by WEBKINDER
  * Plugin URI: https://wordpress.org/plugins/wk-google-analytics/
  * Description: Deploy Google Analytics on your website without having to edit code and without tracking your own visits. You can exclude any logged in user from this and enable tracking solely for them.
- * Version: 1.9.8
+ * Version: 1.9.9
  * Author: WEBKINDER
  * Author URI: https://www.webkinder.ch
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Domain Path: /languages
- * Text Domain: wk-google-analytics
+ * Text Domain: wk-google-analytics.
  */
-
-
 define('WK_GOOGLE_ANALYTICS_DIR', dirname(__FILE__));
 
-$autoload = __DIR__ . '/vendor/autoload.php';
+$autoload = __DIR__.'/vendor/autoload.php';
 if (file_exists($autoload)) {
-	require_once($autoload);
+	require_once $autoload;
 }
 
 WebKinder\GoogleAnalytics\PluginFactory::create()->run();
