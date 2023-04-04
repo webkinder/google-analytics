d23a27fbae4e805cd4e676e9a9ca3eebf6832d6e
Raphael Portmann <raphael@webkinder.ch>
2023-04-04 13:53:12 +0200

    Changed gtm script url to always use https

--- lib/Loader.php
+++ lib/Loader.php
@@ -47,7 +47,7 @@ var f = d.getElementsByTagName(s)[0],
 j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
 j.async = true;
 j.src =
-'//www.googletagmanager.com/gtm.js?id=' + i + dl;
+'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
 f.parentNode.insertBefore(j, f);
 })(window, document, 'script', 'dataLayer', '<?php echo $tag_manager_id; ?>');
 }
